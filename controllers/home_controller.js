e990f0182c3321a3f687175de3548cbb15f4f4e1
Akhilesh17-Tech <akhilesh.tech99@gmail.com>
2022-03-28 07:43:19 +0530

    Issue_tracker project first commit

---
+++ controllers/home_controller.js
@@ -0,0 +1,16 @@
+const Project = require("../models/project");
+const Issue = require("../models/issue");
+
+module.exports.home = async function (req, res) {
+  try {
+    // CHANGE :: populate the likes of each post and comment
+    let projects = await Project.find({});
+    return res.render("home", {
+      title: "Issue_Tracker | Home",
+      projects: projects,
+    });
+  } catch (err) {
+    console.log("Error", err);
+    return;
+  }
+};
---
+++ controllers/project_controller.js
@@ -0,0 +1,84 @@
+const Project = require("../models/project");
+const Issue = require("../models/issue");
+
+module.exports.createproject = function (req, res) {
+  return res.render("create_project", {
+    title: "IssueTraker | create_project",
+  });
+};
+
+module.exports.create = function (req, res) {
+  let project = Project.create(
+    {
+      name: req.body.name,
+      description: req.body.description,
+      author: req.body.author,
+    },
+    function (err, project) {
+      if (err) {
+        req.flash("error", "Please check!");
+        return;
+      }
+    }
+  );
+  req.flash("success", "Project created!");
+  return res.redirect("back");
+};
+
+module.exports.project = function (req, res) {
+  Project.findById(req.params.id)
+    .populate("issue")
+    .exec(function (err, project) {
+      // do something.
+      // variable `person` contains the final populated data
+      if (err) {
+        console.log(err);
+        return;
+      }
+
+      return res.render("project", {
+        title: "IssueTraker | Projects",
+        project: project,
+      });
+    });
+};
+
+module.exports.createIssue = function (req, res) {
+  Project.findById(req.params.id)
+    .populate("issue")
+    .exec(function (err, project) {
+      if (err) {
+        return;
+      }
+      // console.log(project);
+      return res.render("create_issue", {
+        title: "IssueTraker | create_issue",
+        project: project,
+      });
+    });
+};
+
+module.exports.projectIssue = async function (req, res) {
+  try {
+    let project = await Project.findById(req.body.project);
+
+    if (project) {
+      let issue = await Issue.create({
+        title: req.body.title,
+        description: req.body.description,
+        lables: req.body.lables,
+        author: req.body.author,
+        project: req.body.project,
+      });
+
+      project.issue.push(issue);
+      project.save();
+
+      req.flash("success", "issue created!");
+      res.redirect("back");
+    }
+  } catch (err) {
+    req.flash("error", err);
+    return;
+  }
+};
